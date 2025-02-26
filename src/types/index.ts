6beedde83e3871612092419f1bc27c6b7341130e
SenyashaLaptopik <arharit27@gmail.com>
2025-02-26 08:50:51 +0300

    readme

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,41 +1,49 @@
-export interface IProductItem {
+import { IEvents } from "../components/base/events";
+
+export interface ICart {
+  items: Map<string, number>;
+  add(id: string): void;
+  remove(id: string): void;
+}
+
+export interface ICatalog {
+  items: IProduct;
+  setItems(items: IProduct[]): void;
+  getProduct(id: string): IProduct;
+}
+
+export interface IProduct {
     id: string;
     description: string;
     image: string;
     title: string;
     category: string;
     price: number | null;
-  }
-  
-  export interface IActions {
-    onClick: (event: MouseEvent) => void;
-  }
-  
-  export interface IOrderForm {
-  payment?: string;
-  address?: string;
-  phone?: string;
-  email?: string;
-  total?: string | number;
-  }
-  
-  export interface IOrder extends IOrderForm {
+}
+
+export interface IOrderForm {
+    payment?: string;
+    address?: string;
+    phone?: string;
+    email?: string;
+    total?: string | number;
+}
+
+export interface IOrder extends IOrderForm {
     items: string[];
-  }
-  
-  export interface IOrderLot{
-    payment: string;
-    email: string;
-    phone: string;
-    address: string;
-    total: number;
-    items: string[];
-  }
-  
-  export interface IOrderResult {
+}
+
+export type OrderError = Partial<Record<keyof IOrder, string>>;
+
+export interface IOrderResponse {
     id: string;
     total: number;
-  }
-  
-  // тип ошибки формы
-  export type FormErrors = Partial<Record<keyof IOrder, string>>;
+}
+
+export interface IView {
+    render(data?: object): HTMLElement;
+}
+
+export interface IViewConstructor {
+    new(container: HTMLElement, events?: IEvents): IView;
+}
